6bb3c830212ebf51301ceedd89a67f66c46934c2
bootloopmaster636 <christhartono@hotmail.com>
2024-04-02 09:22:04 +0700

    ui/component/logo: add ability to hide brand and add dynamic text color

--- lib/general_components.dart
+++ lib/general_components.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:p3l_k3_mobile/constants.dart';
 
 class LogoAtmaKitchen extends StatelessWidget {
-  const LogoAtmaKitchen({required this.type, required this.height, super.key});
+  const LogoAtmaKitchen({required this.type, required this.showBrand, required this.height, super.key});
   final LogoType type;
+  final bool showBrand;
   final double height;
 
   @override
@@ -13,6 +14,7 @@ class LogoAtmaKitchen extends StatelessWidget {
         : type == LogoType.black
             ? 'assets/logo/black.png'
             : 'assets/logo/white.png';
+    final Color textColor = type == LogoType.color || type == LogoType.black ? Colors.black : Colors.white;
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
@@ -21,19 +23,22 @@ class LogoAtmaKitchen extends StatelessWidget {
           height: height,
           width: height,
         ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Atma Kitchen',
-              style: TextStyle(fontSize: 16 * (height / 60), fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'Since 2024 - Cake & Drinks',
-              style: TextStyle(fontSize: 12 * (height / 60), fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+        if (showBrand)
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Atma Kitchen',
+                style: TextStyle(fontSize: 16 * (height / 60), fontWeight: FontWeight.bold, color: textColor),
+              ),
+              Text(
+                'Since 2024 - Cake & Drinks',
+                style: TextStyle(fontSize: 12 * (height / 60), fontWeight: FontWeight.bold, color: textColor),
+              ),
+            ],
+          )
+        else
+          const SizedBox(),
       ],
     );
   }
