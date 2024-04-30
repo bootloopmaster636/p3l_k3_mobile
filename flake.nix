8f3048a22935e989fef959f82c65238d03cd9198
bootloopmaster636 <christhartono@hotmail.com>
2024-04-30 13:50:43 +0700

    nix: update flake for the last time :')

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "Flutter environment";
+  # see this https://manuelplavsic.ch/articles/flutter-environment-with-nix/ for reference
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
@@ -17,15 +18,14 @@
         androidEnv = pkgs.androidenv.override { licenseAccepted = true; };
         androidComposition = androidEnv.composeAndroidPackages {
           cmdLineToolsVersion = "8.0"; # emulator related: newer versions are not only compatible with avdmanager
-          platformToolsVersion = "33.0.3";
-          buildToolsVersions = [ "33.0.3" ];
-          platformVersions = [ "33" ];
-          abiVersions = [ "x86_64" ]; # emulator related: on an ARM machine, replace "x86_64" with
-          # either "armeabi-v7a" or "arm64-v8a", depending on the architecture of your workstation.
+          platformToolsVersion = "34.0.1";
+          buildToolsVersions = [ "30.0.3" "33.0.2" "34.0.0"  ];
+          platformVersions = [ "29" "31" "32" "33" "34" ];
+          abiVersions = [ "x86_64" ]; 
           includeNDK = false;
           includeSystemImages = false; # emulator related: system images are needed for the emulator.
           systemImageTypes = [ "google_apis" "google_apis_playstore" ];
-          includeEmulator = false; # emulator related: if it should be enabled or not
+          includeEmulator = false;
           useGoogleAPIs = true;
           extraLicenses = [
             "android-googletv-license"
@@ -43,24 +43,19 @@
         devShell = with pkgs; mkShell rec {
           ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
           ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
-          JAVA_HOME = jdk21.home;
+          JAVA_HOME = jdk17.home;
           FLUTTER_ROOT = flutter;
           DART_ROOT = "${flutter}/bin/cache/dart-sdk";
           GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/33.0.2/aapt2";
           QT_QPA_PLATFORM = "wayland;xcb"; # emulator related: try using wayland, otherwise fall back to X.
-          # NB: due to the emulator's bundled qt version, it currently does not start with QT_QPA_PLATFORM="wayland".
-          # Maybe one day this will be supported.
           buildInputs = [
             androidSdk
             flutter
             qemu_kvm
             gradle
-            jdk21
+            jdk17
           ];
-          # emulator related: vulkan-loader and libGL shared libs are necessary for hardware decoding
           LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [vulkan-loader libGL]}";
-          # Globally installed packages, which are installed through `dart pub global activate package_name`,
-          # are located in the `$PUB_CACHE/bin` directory.
           shellHook = ''
             if [ -z "$PUB_CACHE" ]; then
               export PATH="$PATH:$HOME/.pub-cache/bin"
