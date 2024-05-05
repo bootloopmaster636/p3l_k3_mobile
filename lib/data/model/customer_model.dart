813eb48ae92369bdd7e349f5e82c81bdf9a610c8
bootloopmaster636 <christhartono@hotmail.com>
2024-05-05 15:23:14 +0700

    feat: add customer model

---
+++ lib/data/model/customer_model.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:p3l_k3_mobile/data/model/user_model.dart';
+
+part 'customer_model.freezed.dart';
+part 'customer_model.g.dart';
+
+@freezed
+class Customer with _$Customer {
+  const factory Customer({
+    required int id,
+    required int point,
+    @JsonKey(name: 'nominal_balance') required int nominalBalance,
+    @JsonKey(name: 'users') required User user,
+    @Default(0)
+    int userId, // might not be useful.. just comment it if it makes trouble
+  }) = _Customer;
+
+  factory Customer.fromJson(Map<String, dynamic> json) =>
+      _$CustomerFromJson(json);
+}
---
+++ lib/data/model/customer_model.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'customer_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$CustomerImpl _$$CustomerImplFromJson(Map<String, dynamic> json) =>
+    _$CustomerImpl(
+      id: json['id'] as int,
+      point: json['point'] as int,
+      nominalBalance: json['nominal_balance'] as int,
+      user: User.fromJson(json['users'] as Map<String, dynamic>),
+      userId: json['userId'] as int? ?? 0,
+    );
+
+Map<String, dynamic> _$$CustomerImplToJson(_$CustomerImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'point': instance.point,
+      'nominal_balance': instance.nominalBalance,
+      'users': instance.user,
+      'userId': instance.userId,
+    };
