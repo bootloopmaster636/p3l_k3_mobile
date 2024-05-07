cfa1eb437da3764973783e2ccd52e6aa41393a4b
bootloopmaster636 <christhartono@hotmail.com>
2024-05-07 16:25:51 +0700

    ui: add toast and validation to forgot password

--- lib/screen/auth/forgot_password.dart
+++ lib/screen/auth/forgot_password.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gap/gap.dart';
 import 'package:logger/logger.dart';
 import 'package:p3l_k3_mobile/data/api/forgotPasswordApi.dart';
 import 'package:p3l_k3_mobile/router.dart';
+import 'package:toastification/toastification.dart';
 
 @RoutePage()
 class ForgotPasswordScreen extends HookWidget {
@@ -56,32 +58,42 @@ class EnterOTPScreen extends HookWidget {
     final TextEditingController otpCtl = useTextEditingController();
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('Enter the OTP code that has been send to your email'),
-          TextField(
-            controller: otpCtl,
-            decoration: const InputDecoration(
-              hintText: 'OTP code',
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Enter the OTP code that has been send to your email',
+              style: Theme.of(context).textTheme.headlineSmall,
             ),
-          ),
-          FilledButton(
-            onPressed: () async {
-              await VerifyOTP(otpCtl.text).then((value) {
-                Logger().i(value);
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => EnterNewPasswordScreen(
-                      token: value,
+            const Gap(8),
+            TextField(
+              controller: otpCtl,
+              decoration: const InputDecoration(
+                hintText: 'OTP code',
+              ),
+              style: Theme.of(context).textTheme.titleLarge,
+              textAlign: TextAlign.center,
+            ),
+            const Gap(16),
+            FilledButton(
+              onPressed: () async {
+                await VerifyOTP(otpCtl.text).then((value) {
+                  Logger().i(value);
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => EnterNewPasswordScreen(
+                        token: value,
+                      ),
                     ),
-                  ),
-                );
-              });
-            },
-            child: const Text('Done'),
-          ),
-        ],
+                  );
+                });
+              },
+              child: const Text('Done'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -98,31 +110,87 @@ class EnterNewPasswordScreen extends HookWidget {
         useTextEditingController();
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('Enter the OTP code that has been send to your email'),
-          TextField(
-            controller: newPasswordCtl,
-            decoration: const InputDecoration(
-              hintText: 'New password',
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Enter the OTP code that has been send to your email',
+              style: Theme.of(context).textTheme.headlineSmall,
             ),
-          ),
-          TextField(
-            controller: newPasswordConfirmCtl,
-            decoration: const InputDecoration(
-              hintText: 'Confirm new password',
+            TextField(
+              controller: newPasswordCtl,
+              decoration: const InputDecoration(
+                hintText: 'New password',
+              ),
             ),
-          ),
-          FilledButton(
-            onPressed: () async {
-              await EnterNewPassword(newPasswordCtl.text, token).then((value) {
-                context.router.push(const LoginRoute());
-              });
-            },
-            child: const Text('Change password'),
-          ),
-        ],
+            TextField(
+              controller: newPasswordConfirmCtl,
+              decoration: const InputDecoration(
+                hintText: 'Confirm new password',
+              ),
+            ),
+            const Gap(16),
+            FilledButton(
+              onPressed: () async {
+                if (newPasswordCtl.text != newPasswordConfirmCtl.text) {
+                  Logger().d('Passwd not match');
+                  toastification.show(
+                    context: context,
+                    type: ToastificationType.error,
+                    style: ToastificationStyle.flat,
+                    title: const Text('Password does not match'),
+                    description: const Text(
+                      'Please confirm that you enter the same password in each field.',
+                    ),
+                    alignment: Alignment.bottomCenter,
+                    autoCloseDuration: const Duration(seconds: 5),
+                    icon: const Icon(Icons.info_outline),
+                    boxShadow: lowModeShadow,
+                    dragToClose: true,
+                  );
+                  return;
+                }
+                if (newPasswordCtl.text.length < 8) {
+                  toastification.show(
+                    context: context,
+                    type: ToastificationType.error,
+                    style: ToastificationStyle.flat,
+                    title: const Text('Insecure password'),
+                    description: const Text(
+                      'Password must contain at least 8 characters.',
+                    ),
+                    alignment: Alignment.bottomCenter,
+                    autoCloseDuration: const Duration(seconds: 5),
+                    icon: const Icon(Icons.info_outline),
+                    boxShadow: lowModeShadow,
+                    dragToClose: true,
+                  );
+                }
+                await EnterNewPassword(newPasswordCtl.text, token)
+                    .then((value) {
+                  toastification.show(
+                    context: context,
+                    type: ToastificationType.success,
+                    style: ToastificationStyle.flat,
+                    title: const Text('Change password success'),
+                    description: const Text(
+                      'Dont forget to remember your new password :)',
+                    ),
+                    alignment: Alignment.bottomCenter,
+                    autoCloseDuration: const Duration(seconds: 5),
+                    icon: const Icon(Icons.check_circle_outline),
+                    boxShadow: lowModeShadow,
+                    dragToClose: true,
+                  );
+                  context.router.push(const LoginRoute());
+                });
+              },
+              child: const Text('Change password'),
+            ),
+          ],
+        ),
       ),
     );
   }
